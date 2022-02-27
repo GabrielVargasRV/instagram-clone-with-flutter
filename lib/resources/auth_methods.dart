35376b7b123caade627475c58e6581c5852a913d
Gabriel Vargas <josegabrielrosariovarg@gmail.com>
2022-02-27 12:59:53 -0400

    user model

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:insta/models/user.dart' as model;
 import 'dart:typed_data';
 
 import 'package:insta/resources/storage_methods.dart';
@@ -29,19 +30,20 @@ class AuthMethods{
           //register user
           UserCredential cred = await _auth.createUserWithEmailAndPassword(email:email,password: password);
           
-          print(cred.user!.uid);
 
           String photoUrl = await StorageMethods().uploadImageToStorage('profilePics', file, false);
 
-          await _firestore.collection('users').doc(cred.user!.uid).set({
-            'username': username,
-            'uid': cred.user!.uid,
-            'email': email,
-            'bio': bio,
-            'followers': [],
-            'following': [],
-            'photoUrl': photoUrl
-          });
+          model.User user = model.User(
+            username: username,
+            uid: cred.user!.uid,
+            email: email,
+            bio: bio,
+            photoUrl: photoUrl,
+            followers: [],
+            following: []
+          );
+
+          await _firestore.collection('users').doc(cred.user!.uid).set(user.toJson());
 
           res = "success";
       }
